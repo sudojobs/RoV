aceb04fc2203b451e4e2dc99357a43192314d72c
sudojobs <shobjobs@gmail.com>
2024-04-28 19:50:21 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,5 +1,2 @@
 #!/bin/bash
-
-# Start streaming video from Raspberry Pi camera module
-raspivid -o - -t 0 -n -w 1280 -h 720 -fps 30 | \
-ffmpeg -i - -vcodec copy -an -f rtsp -rtsp_transport tcp rtsp://localhost:8554/unicast
+libcamera-vid -t 0 --inline --width 1920 --height 1080 --framerate 15 -o - | cvlc -vvv stream:///dev/stdin --sout '#rtp{sdp=rtsp://:8554/stream}' :demux=h264
